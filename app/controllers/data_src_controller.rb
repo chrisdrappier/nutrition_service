6881711619b42a9c79f7b8590b6ef0217ae3bf01
Chris Drappier <chris.drappier@gmail.com>
2017-05-10 02:06:45 -0500

    WIP parse delimited file via File before trying CSV

--- app/controllers/data_src_controller.rb
+++ app/controllers/data_src_controller.rb
@@ -1,10 +1,20 @@
+# controllerfor DataSrc resource
 class DataSrcController < ApplicationController
-
   def show
-
+    @data_src = CSV.read('/Users/chris/sr28asc/DATA_SRC-copy.txt',
+                         col_sep: '^',
+                         row_sep: '\r\n',
+                         quote_char: '~').to_json
+    render json: @data_src
   end
 
   def index
+    @data_src = []
 
+    File.foreach('/Users/chris/sr28asc/DATA_SRC.txt') do |row|
+      @data_src << row.split('^').collect { |cell| cell.gsub('~', '').gsub(/\r\n/, '') }
+    end
+
+    render json: @data_src
   end
 end
